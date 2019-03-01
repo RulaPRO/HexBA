c9197cfce42bf185fc0f86d1b638bc41a88a7b21
RulaPRO <d2rulezzz@gmail.com>
2019-03-01 23:35:53 +0300

    Метрики шестиугольника

---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HexMetrics : MonoBehaviour
+{
+    public const float OuterRadius = 10f;
+    public const float InnerRadius = OuterRadius * 0.866025404f;
+
+    public static Vector3[] Corners = {
+        new Vector3(0f, 0f, OuterRadius),
+        new Vector3(InnerRadius, 0f, 0.5f * OuterRadius),
+        new Vector3(InnerRadius, 0f, -0.5f * OuterRadius),
+        new Vector3(0f, 0f, -OuterRadius),
+        new Vector3(-InnerRadius, 0f, -0.5f * OuterRadius),
+        new Vector3(-InnerRadius, 0f, 0.5f * OuterRadius),
+        new Vector3(0f, 0f, OuterRadius)
+    };
+}
