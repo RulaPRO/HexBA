9ed9eea1160aab875de89c838394bed0bd8bd344
RulaPRO <d2rulezzz@gmail.com>
2019-03-02 14:36:40 +0300

    Отображение координат

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
@@ -6,11 +7,15 @@ public class HexGrid : MonoBehaviour
     public int Height = 6;
 
     public HexCell CellPrefab;
+    public Text HexCellLabelPrefab;
 
     private HexCell[] _cells;
+    private Canvas _gridCanvas;
 
     void Awake()
     {
+        _gridCanvas = GetComponentInChildren<Canvas>();
+        
         _cells = new HexCell[Height * Width];
 
         for (int z = 0, i = 0; z < Height; z++) {
@@ -27,8 +32,13 @@ public class HexGrid : MonoBehaviour
         position.y = 0f;
         position.z = z * 10f;
 
-        HexCell cell = _cells[i] = Instantiate(CellPrefab);
-        cell.transform.SetParent(transform, false);
-        cell.transform.localPosition = position;
+        HexCell hexCell = _cells[i] = Instantiate(CellPrefab);
+        hexCell.transform.SetParent(transform, false);
+        hexCell.transform.localPosition = position;
+        
+        Text labelText = Instantiate(HexCellLabelPrefab);
+        labelText.rectTransform.SetParent(_gridCanvas.transform, false);
+        labelText.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        labelText.text = x + "\n" + z;
     }
 }
