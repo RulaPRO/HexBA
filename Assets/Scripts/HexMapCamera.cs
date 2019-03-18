c326b8ead03d1954938aaff2eb9db0c908efe84f
RulaPRO <d2rulezzz@gmail.com>
2019-03-18 21:21:05 +0300

    Иерархия камеры

---
+++ Assets/Scripts/HexMapCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class HexMapCamera : MonoBehaviour
+{
+    private Transform _swivel;
+    private Transform _stick;
+    
+    private void Awake()
+    {
+        _swivel = transform.GetChild(0);
+        _stick = _swivel.GetChild(0);
+    }
+}
